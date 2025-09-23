571ef6335d90f9476a0e32769c93b5ac76b705e3
ajitdevv <ajitdev014@gmail.com>
2025-09-23 13:55:22 +0530

    Responsivenes complet completely

--- New folder/3.js
+++ New folder/3.js
@@ -1,6 +1,10 @@
+// let a = 12;
+// {
+//     let a=13;
+//     console.log("inside:", a);
+// }
+// console.log("outside:",a);
 let a = 12;
-{
-    let a=13;
-    console.log("inside:", a);
-}
-console.log("outside:", a);
+
+let b=a;
+a = a+2;
