7fed60599fc432e84caffd9184d112dfb6d2cc50
ajitdevv <ajitdev014@gmail.com>
2025-09-27 18:57:34 +0530

    make toaste funclity

--- New folder/3.js
+++ New folder/3.js
@@ -4,7 +4,42 @@
 //     console.log("inside:", a);
 // }
 // console.log("outside:",a);
-let a = 12;
+// let a = 12;
 
-let b=a;
-a = a+2;
+// let b=a;
+// a = a+2;
+
+
+// * lexical scoping ---> ki aap kha pr phycicale avalible ho ye totely depend krta h ki aap kya access kr paoge ,( )this his follow in js
+ 
+
+// * dynamic scoping ---> kha se call kr rhe ho us pr depend krta h ki kya value milegi
+
+// * closuers --->  closures hote h function but wo function jo kisi perent function ko ke kisi varbile ko print kr rha ho  
+// ? advantages --->  ues to this make privet varibles
+// closuers are make a backlinks of variables, use  of [[environment]]
+
+
+
+
+// toster notifiction
+
+function createToster(confe) {
+return function (str) {
+  let div  = document.createElement("div");
+  div.textContent= str;
+  div.className="inline-block bg-gray-800 text-white px-6 py-3 rounded shadow-lg pointer-events-none transition-opacity duration-300"
+  document.querySelector(".toast").appendChild(div);
+
+};
+    
+}
+
+let toster = createToster({
+    positionX:"right",
+    positionY:"top",
+    theme:"dark",
+    duration:3,
+     
+});
+toster("downlod now")
