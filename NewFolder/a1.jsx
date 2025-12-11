5ace8ba12a086c738f3ac7bfe922b8e5bf5c0e8b
ajitdevv <ajitdev014@gmail.com>
2025-12-11 10:49:28 +0530

    commit

---
+++ NewFolder/a1.jsx
@@ -0,0 +1,72 @@
+ <div className="relative">
+        <div className="w-full flex flex-col items-center py-10 relative">
+          <h1
+            data-aos="fade-up"
+            data-aos-offset="50"
+            className="text-4xl font-bold mb-10 text-primary"
+          >
+            Start
+          </h1>
+          <div className="w-1 bg-accent h-[432px] lg:h-[632px] relative"></div>
+          <div className="size-4 rounded-full bg-accent top-28 absolute"></div>
+          <div className="absolute top-40 lg:top-50 left-1/2 flex items-center gap-4">
+            <div className="w-20 sm:w-57 lg:w-65 h-1 bg-accent"></div>
+            <h2
+              data-aos="fade-left"
+              data-aos-offset="70"
+              className="text-xl sm:text-2xl font-semibold text-primary"
+            >
+              HTML5
+            </h2>
+          </div>
+          <div className="absolute top-60 lg:top-80 right-1/2 flex items-center gap-4">
+            <h2
+              data-aos="fade-right"
+              data-aos-offset="70"
+              className="text-xl sm:text-2xl font-semibold text-primary"
+            >
+              CSS 3
+            </h2>
+            <div className="w-20 sm:w-57 lg:w-65 h-1 bg-accent"></div>
+          </div>
+          <div className="absolute top-80 lg:top-110 left-1/2 flex items-center gap-4">
+            <div className="w-20 sm:w-57 lg:w-65 h-1 bg-accent"></div>
+            <h2
+              data-aos="fade-left"
+              data-aos-offset="70"
+              className="text-xl sm:text-2xl font-semibold text-primary"
+            >
+              JavaScript
+            </h2>
+          </div>
+
+          <div className="absolute top-100 lg:top-140 right-1/2 flex items-center gap-4">
+            <h2
+              data-aos="fade-right"
+              data-aos-offset="70"
+              className="text-xl sm:text-2xl font-semibold text-primary"
+            >
+              React.js
+            </h2>
+            <div className="w-20 sm:w-57 lg:w-70 h-1 bg-accent"></div>
+          </div>
+
+          <div className="absolute top-120 lg:top-160 left-1/2 flex items-center gap-4">
+            <div className="w-20 sm:w-57 lg:w-65 h-1 bg-accent"></div>
+            <h2
+              data-aos="fade-left"
+              data-aos-offset="70"
+              className="text-xl sm:text-2xl font-semibold text-primary"
+            >
+              Tailwind CSS
+            </h2>
+          </div>
+          <h1
+            data-aos="fade-down"
+            data-aos-offset="0"
+            className="absolute bottom-0 text-3xl font-bold text-primary"
+          >
+            Progress..
+          </h1>
+        </div>
+      </div> 
